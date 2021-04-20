5b20e0ebc8e93c0d92f5496218adaae218a38dd1
John Medeiros <78372567+JMedeiros-dev@users.noreply.github.com>
2021-04-20 11:53:54 -0400

    Add files via upload

---
+++ MVC/MVC/src/main/java/dev/johnmedeiros/mvc/services/BookService.java
@@ -0,0 +1,59 @@
+package dev.johnmedeiros.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Service;
+
+import dev.johnmedeiros.mvc.models.Book;
+import dev.johnmedeiros.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+
+	private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    // updates a book
+    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+    	Book book = findBook(id);
+			book.setTitle(title);
+			book.setDescription(desc);
+			book.setLanguage(lang);
+			book.setNumberOfPages(numOfPages);
+			return bookRepository.save(book);
+    }
+    
+    // deletes a book
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById(id);
+    }
+	public void updateBook(@Valid Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+    
+
+}
