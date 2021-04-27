ce6af591ca851af9328a014a02b3536ddeaf09aa
John Medeiros <78372567+JMedeiros-dev@users.noreply.github.com>
2021-04-27 10:06:40 -0400

    Add files via upload

---
+++ Strings/src/test/java/dev/johnmedeiros/strings/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnmedeiros.strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
