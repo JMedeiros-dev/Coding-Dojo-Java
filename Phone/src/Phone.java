db228bc135a00e2181aa12bd272437c946e91b32
John Medeiros <78372567+JMedeiros-dev@users.noreply.github.com>
2021-04-28 16:52:28 -0400

    Update Phone.java

--- Phone/src/Phone.java
+++ Phone/src/Phone.java
@@ -9,56 +9,38 @@ public abstract class Phone {
         this.carrier = carrier;
         this.ringTone = ringTone;
     }
-    
-    
-    
+   
     public String getVersionNumber() {
 		return versionNumber;
 	}
-
-
 
 	public void setVersionNumber(String versionNumber) {
 		this.versionNumber = versionNumber;
 	}
 
-
-
 	public int getBatteryPercentage() {
 		return batteryPercentage;
 	}
-
-
 
 	public void setBatteryPercentage(int batteryPercentage) {
 		this.batteryPercentage = batteryPercentage;
 	}
 
-
-
 	public String getCarrier() {
 		return carrier;
 	}
-
-
 
 	public void setCarrier(String carrier) {
 		this.carrier = carrier;
 	}
 
-
-
 	public String getRingTone() {
 		return ringTone;
 	}
 
-
-
 	public void setRingTone(String ringTone) {
 		this.ringTone = ringTone;
 	}
-
-
 
 	public abstract void displayInfo();
     
