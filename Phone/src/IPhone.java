493f15c2126164e6929393106edd417e72f55b63
John Medeiros <78372567+JMedeiros-dev@users.noreply.github.com>
2021-04-28 16:50:14 -0400

    Add files via upload

---
+++ Phone/src/IPhone.java
@@ -0,0 +1,18 @@
+public class IPhone extends Phone implements Ringable {
+    public IPhone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+    @Override
+    public String ring() {
+        return "Iphone X says " + this.getRingTone();
+    }
+    @Override
+    public String unlock() {
+    	return "Unlocking via facial recognition";
+    }
+    @Override
+    public void displayInfo() {
+    	System.out.println("Iphone " + this.getVersionNumber() + "from " + this.getCarrier());          
+    }
+}
+
